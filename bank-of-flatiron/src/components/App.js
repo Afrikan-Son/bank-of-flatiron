80b2043bfca78e30c74c0d44da0a71d69002204c
Mark Wanjau <theafrikanson@gmail.com>
2023-05-01 19:21:43 +0300

    app.js added

---
+++ bank-of-flatiron/src/components/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "../app.css";
+import Home from "./Home";
+
+function App() {
+	return (
+		<div className="">
+			<div className="title-div">
+				<h2 className="page-name">The Royal Bank of Flatiron</h2>
+			</div>
+			<Home />
+		</div>
+	);
+}
+
+export default App;
